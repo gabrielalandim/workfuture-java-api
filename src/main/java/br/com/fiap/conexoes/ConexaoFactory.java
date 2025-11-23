1b49a5d42671112a9ce71800b32450e4d1b7320e
Seu Nome <seu-email@exemplo.com>
2025-11-23 20:54:53 -0300

    fix: correção da leitura das variaveis de ambiente no conexaofactory

--- src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
+++ src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
@@ -8,8 +8,13 @@ public class ConexaoFactory {
     public Connection conexao() throws ClassNotFoundException, SQLException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
 
-        String user = System.getenv("RM565146") != null ? System.getenv("RM565146") : "RM565146";
-        String pass = System.getenv("170806") != null ? System.getenv("170806") : "170806";
+        // CORREÇÃO AQUI:
+        // 1. getenv("DB_USER") -> Procura a chave 'DB_USER' que configuramos no Render.
+        // 2. Se não achar (null), usa o seu RM fixo (para rodar no seu PC).
+        String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "RM565146";
+
+        // Mesma coisa para a senha
+        String pass = System.getenv("DB_PASS") != null ? System.getenv("DB_PASS") : "170806";
 
         return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl", user, pass);
     }
